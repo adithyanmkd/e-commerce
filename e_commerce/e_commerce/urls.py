269bc213538ece2bf54ed0dd25d7a60197247c60
adithyanmkd <adithyanmkd123@gmail.com>
2024-02-14 08:59:17 +0530

    cart page added

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls'))
+    path('', include('products.urls')),
+    path('customers/', include('customers.urls')), 
+    path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
