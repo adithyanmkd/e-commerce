dfbe504475771f92c022e2a99105c906c72181b7
adhi mac <adithyanmkd123@gmail.com>
2024-03-01 14:31:14 +0530

    add products to cart added

--- e_commerce/orders/views.py
+++ e_commerce/orders/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Order, OrderedItem
+from products.models import Product
 
-# Create your views here.
+# cart page
 def cart(request):
-    return render(request, 'cart.html')
+    user = request.user
+    customer = user.customer_profile
+    cart_obj, created = Order.objects.get_or_create(
+        owner = customer,
+        order_status = Order.CART_STAGE
+    )
+    item_dict = {'cart':cart_obj}
+    return render(request, 'cart.html', item_dict)
+
+def add_to_cart(request):
+    method = request.POST
+    if method:
+        print(f"{method}============")
+        user = request.user
+        customer = user.customer_profile
+        quantity = method.get('quantity')
+        product_id = method.get('item_id')
+        print(f"{product_id}========>>>>>>>>>")
+        cart_obj, created = Order.objects.get_or_create(
+            owner = customer,
+            order_status = Order.CART_STAGE
+        )
+        product = Product.objects.get(pk=product_id)
+        print(f">>>>>>>>>>============")
+        orderd_item = OrderedItem.objects.create(
+            product = product,
+            quantity = int(quantity),
+            owner = cart_obj,
+        )
+        return redirect('cart')
